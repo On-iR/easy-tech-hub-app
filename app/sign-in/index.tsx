0591339bf2a2faafc0533a995c57aac75e691a4b
nophakhun <nophakhun@superboy.jp>
2024-03-17 03:57:12 +0900

    キーボードまわりの制御を追加

--- app/sign-in/index.tsx
+++ app/sign-in/index.tsx
@@ -13,9 +13,12 @@ import {
   Image,
   Center,
   Heading,
+  KeyboardAvoidingView,
+  Pressable,
 } from "@gluestack-ui/themed";
 import { useState } from "react";
 import { router } from "expo-router";
+import { Keyboard, Platform } from "react-native";
 
 const logo = require("../../assets/images/sign-in-logo.jpg");
 
@@ -28,44 +31,69 @@ export default function SignIn() {
   };
 
   return (
-    <Box flex={1} justifyContent="center" alignItems="center">
-      <FormControl maxWidth="$80" rounded="$md">
-        <VStack space="2xl">
-          <Center>
-            <Image h="$80" w="$80" alt="sign-in-logo" source={logo} />
-          </Center>
-          <VStack space="xs">
-            <Heading size="sm" color="$textLight900">
-              E-mail
-            </Heading>
-            <Input borderColor="$borderLight500">
-              <InputField type="text" />
-            </Input>
-          </VStack>
-          <VStack space="xs">
-            <Heading size="sm" color="$textLight900">
-              Password
-            </Heading>
-            <Input borderColor="$borderLight500">
-              <InputField type={showPassword ? "text" : "password"} />
-              <InputSlot pr="$3" onPress={handleState}>
-                <InputIcon
-                  as={showPassword ? EyeIcon : EyeOffIcon}
-                  color="$darkBlue500"
-                />
-              </InputSlot>
-            </Input>
-          </VStack>
-          <Button
-            ml="auto"
-            onPress={() => {
-              router.push("/home/");
-            }}
-          >
-            <ButtonText color="$white">SignIn</ButtonText>
-          </Button>
-        </VStack>
-      </FormControl>
-    </Box>
+    // キーボード外をタップしてキーボードをしまえるようにする
+    <Pressable
+      flex={1}
+      onPress={() => {
+        // 入力ができなくなるためWebでは実行しない
+        if (Platform.OS === "ios" || Platform.OS === "android") {
+          Keyboard.dismiss();
+        }
+      }}
+    >
+      {/* キーボード入力時に入力欄が見えるようにする */}
+      <KeyboardAvoidingView
+        flex={1}
+        // TODO: iosでメアド入力時に画面がガタガタする現象を無くしたい
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <Box flex={1} justifyContent="center" alignItems="center">
+          <FormControl maxWidth="$80" rounded="$md">
+            <VStack space="2xl">
+              <Center>
+                <Image h="$80" w="$80" alt="sign-in-logo" source={logo} />
+              </Center>
+              <VStack space="xs">
+                <Heading size="sm" color="$textLight900">
+                  E-mail
+                </Heading>
+                <Input w="$80" borderColor="$borderLight500">
+                  <InputField
+                    type="text"
+                    keyboardType="email-address"
+                    autoCapitalize="none" // 自動大文字入力をOFF
+                  />
+                </Input>
+              </VStack>
+              <VStack space="xs">
+                <Heading size="sm" color="$textLight900">
+                  Password
+                </Heading>
+                <Input w="$80" borderColor="$borderLight500">
+                  <InputField
+                    type={showPassword ? "text" : "password"}
+                    keyboardType="default"
+                  />
+                  <InputSlot pr="$3" onPress={handleState}>
+                    <InputIcon
+                      as={showPassword ? EyeIcon : EyeOffIcon}
+                      color="$darkBlue500"
+                    />
+                  </InputSlot>
+                </Input>
+              </VStack>
+              <Button
+                ml="auto"
+                onPress={() => {
+                  router.push("/home/");
+                }}
+              >
+                <ButtonText color="$white">SignIn</ButtonText>
+              </Button>
+            </VStack>
+          </FormControl>
+        </Box>
+      </KeyboardAvoidingView>
+    </Pressable>
   );
 }
